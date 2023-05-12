b30820b3080e309abbda5c12cbf0715268c9d9a4
Remy-Tresor-250 <niyitegekatresor@gmail.com>
2023-05-12 20:04:43 +0200

    renewed the logos

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,11 @@
 // import "@fortawesome/fontawesome-svg-core/styles.css";
 import styles from "./styles";
+import Image from "next/image";
 
 
 const Home = () => {
   return (
-    <main className={`bg-lightGray w-[100%] h-[100%] ${styles.flexCenter}`}>
+    <main className={`bg-black w-[100%] h-[100%] ${styles.flexCenter}`}>
       
     </main>
   )
